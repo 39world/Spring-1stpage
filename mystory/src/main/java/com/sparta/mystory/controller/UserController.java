7c612980b79d5616667647a039c7d89963795ab5
Sanggu <rkdtkdrn93@naver.com>
2021-04-01 03:07:32 +0900

    test

---
+++ mystory/src/main/java/com/sparta/mystory/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.sparta.mystory.controller;
+
+import com.sparta.mystory.dto.SignupRequestDto;
+import com.sparta.mystory.model.Story;
+import com.sparta.mystory.model.User;
+import com.sparta.mystory.repository.UserRepository;
+import com.sparta.mystory.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // 회원 로그인 페이지
+    @GetMapping("/user/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/user/login/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
+    }
+
+    // 회원 가입 페이지
+    @GetMapping("/user/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    // 회원 가입 요청 처리
+    @PostMapping("/user/signup")
+    public String registerUser(SignupRequestDto requestDto) {
+        userService.registerUser(requestDto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/user/kakao/callback")
+    public String kakaoLogin(String code) {
+        // authorizedCode: 카카오 서버로부터 받은 인가 코드
+        userService.kakaoLogin(code);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/api/user/{name}")
+    public User checkNm(@PathVariable String name) {
+        User user = UserRepository.findAllByUsername(name);
+        return user;
+    }
+
+}
